19cf46e7ec9b8c58227b5592fa37db173d923796
Surralis <Madera.Jonas@seznam.cz>
2020-06-11 19:36:41 +0200

    Toggle ON !

--- ToDo copy 2/script.js
+++ ToDo copy 2/script.js
@@ -3,11 +3,13 @@
 let a = 1;
 let b = true;
 let c= true
+n = 2
 
 $("#shoppingForm").on('submit', function(e){
 	var text = $('#addToList').val();
   $('#shoppingList').append($('<li>'+text+ '<button class="del">Del</button><button class="done" id="On">Done</button> </li>'));
-
+  $(".done").css("color","black")
+  $(".done").css("background","green")
    $('#addToList').val('');
   e.preventDefault();
   a += 1
@@ -31,15 +33,27 @@ $("#shoppingList").on('click', '.del',  function(){
 
 
   $("#shoppingList").on('click', '.done',  function(){
-    var f = !$(this).data("toggleFlag");
-    if (f) {
-     $(this).parent().css("color","gray")
-     $(this).css("background","gray")
-    } else {
-     $(this).parent().css("color","black")
-     $(this).css("background","yellow")
+    
+
+    console.log(this.style.backgroundColor)
+    if ((this).style.backgroundColor === "green") {
+    $(this).parent().css("color","gray");
+    $(this).css("background","gray");
+   
+    } 
+ else {
+  console.log(this.style.backgroundColor)
+  $(this).parent().css("color","black")
+  $(this).css("background","green")
   
-    }});
-    $(this).data("toggleFlag", f); 
+ 
+}});
+
+ 
+     
+
+
+  
+ 
 
   
